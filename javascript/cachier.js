4eb680302b0df5753c73bbb3bf93bb2247a153de
mohammad saied taleb <mohammadsolingen@gmail.com>
2019-09-12 10:59:26 +0200

    script

---
+++ javascript/cachier.js
@@ -0,0 +1,14 @@
+
+
+var ArtikelName =["Brot","Wasser","Salz","Apfel"]
+var Preis =[0.5,1.5,0.66,2]
+var kosten
+function cachier(NameVonArtikel,menge){
+for (var i=0;i<ArtikelName.length;i++){
+    if (NameVonArtikel==ArtikelName[i]){
+       kosten=Preis[i]*menge
+            }
+            if()
+}
+return  NameVonArtikel+" kostet "+kosten+" Euro"
+}
