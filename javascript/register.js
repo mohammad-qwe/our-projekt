0ba88ebd68ea3e2f4b5f9cc72251da97799773f0
fatima <benyamna2011@outlook.de>
2019-09-12 11:02:49 +0200

    fatima

---
+++ javascript/register.js
@@ -0,0 +1,16 @@
+function Gesamtsumme(a , b)
+{
+   
+    var S = a + b
+
+    return (S + "Euro");
+
+}
+function Rückgeld(a , b )
+{
+    
+    var S = a + b
+    var G = 10000
+    var R = G - S
+    return (R+ "Euro"); 
+}
