5ebe9d4599861d051e581c56398bb55a8909b466
mohammad saied taleb <mohammadsolingen@gmail.com>
2019-09-23 10:21:07 +0200

    quiz

--- javascript/cachier.js
+++ javascript/cachier.js
@@ -1,31 +1,46 @@
-var summe
+
 var beträge = []
 var nummer=-1
+var kosten=0
 var ArtikelName =["brot","wasser","salz","apfel"]
 var Preis =[0.5,1.5,0.66,2]
-var kosten
 function cachier(NameVonArtikel,menge){
 for (var i=0;i<ArtikelName.length;i++){
     if (NameVonArtikel==ArtikelName[i]){
        kosten=Preis[i]*menge
-
-            }
-            
+            }          
 }
 nummer++
 beträge[nummer]=kosten
 return kosten
+}
+
+
+function reset(){
+    beträge = []
+    nummer=-1 
+    kosten=0
 }
 function Betrag(){
     var betrag=0
 for(var n=0;n<beträge.length;n++){
 betrag=betrag+beträge[n]
 }
-summe=betrag
-return summe
+
+return betrag
 }
 
+function removArtikel(NameVonArtikel,menge){
+    for (var i=0;i<ArtikelName.length;i++){
+        if (NameVonArtikel==ArtikelName[i]){
+           kosten=Preis[i]*menge
+                }          
+    }
+    var totalPreis=Betrag()
 
+    return totalPreis-kosten
+    
+    }
 
 
 
