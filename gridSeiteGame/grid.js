0e2ec8008651f6837a93b18e89ea29fcc455ea25
mohammad saied taleb <mohammadsolingen@gmail.com>
2019-10-01 10:34:51 +0200

    new

--- gridSeiteGame/grid.js
+++ gridSeiteGame/grid.js
@@ -3,18 +3,23 @@ var getTextA = document.getElementById('text1')
 function show1(){
 textA[0].innerHTML=getTextA.innerHTML
 
-
 }
 
 function myFunction1() {
     var txt;
     var antwort = prompt("wähle deine Antwort\na:das Manometer\nb:der Kalender " );
-    if (antwort == "a") {
-      txt = "tschüss falsche Antwort.";
-    } else {
-      txt ="Richtige Antwort bravoo!";
+    if (antwort == "b") {
+    txt ="Richtige Antwort bravoo!";
+    var background1 =document.getElementById('item1');
+    background1.style.background='url(smiley-163510__340.jpg)'
     }
+    else {
+      txt = "tschüss falsche Antwort.";
+      var background1 =document.getElementById('item1');
+      background1.style.background='url(smiley-3058590__340.jpg)'
+    } 
     document.getElementById("text1").innerHTML = txt;
+    
   }
 
   var textB = document.getElementsByClassName('item2')
@@ -29,10 +34,14 @@ function myFunction1() {
   function myFunction2() {
     var txt;
     var antwort = prompt("wähle deine Antwort\na:the Blues\nb:the Blacks " );
-    if (antwort == "b") {
-      txt = "falsche Antwort, 10 mal Liegestütze hhhhhhh";
-    } else {
+    if (antwort == "a") {
+      var background1 =document.getElementById('item2');
+    background1.style.background='url(smiley-163510__340.jpg)'
       txt ="Richtige bravoooooooooo! prima!!!!";
+    } else {
+      var background1 =document.getElementById('item2');
+      background1.style.background='url(smiley-3058590__340.jpg)'
+      txt = "falsche Antwort, 10 mal Liegestütze hhhhhhh";
     }
     document.getElementById("text2").innerHTML = txt;
   }
@@ -47,10 +56,15 @@ textC[0].innerHTML=getTextC.innerHTML
   function myFunction3() {
     var txt;
     var antwort = prompt("wähle deine Antwort\na:Fels \nb:Riff " );
-    if (antwort == "a") {
-      txt = "falsche Antwort Tschüss machst gut nicht viel essen du kriegst Bauchschmerzen.";
-    } else {
+     if(antwort=="b") {
+      var background1 =document.getElementById('item3');
+    background1.style.background='url(smiley-163510__340.jpg)'
       txt ="Richtige Antwort bravoo!";
+    }
+    else {
+      var background1 =document.getElementById('item3');
+      background1.style.background='url(smiley-3058590__340.jpg)'
+      txt = "falsche Antwort Tschüss machst gut nicht viel essen du kriegst Bauchschmerzen.";
     }
     document.getElementById("text3").innerHTML = txt;
   }
@@ -65,11 +79,17 @@ textD[0].innerHTML=getTextD.innerHTML
   function myFunction4() {
     var txt;
     var antwort = prompt("wähle deine Antwort\na:5 \nb:4 " );
-    if (antwort == "a") {
-      txt = "falsche Antwort Tschüss machst gut nicht viel essen du kriegst Bauchschmerzen.";
-    } else {
+    if(antwort=="b") {
+      var background1 =document.getElementById('item4');
+    background1.style.background='url(smiley-163510__340.jpg)'
       txt ="Richtige Antwort mach weiter gleich geschafft!";
+
     }
+    else{
+      var background1 =document.getElementById('item4');
+      background1.style.background='url(smiley-3058590__340.jpg)'
+      txt = "falsche Antwort Tschüss machst gut nicht viel essen du kriegst Bauchschmerzen.";
+    } 
     document.getElementById("text4").innerHTML = txt;
   }
 
@@ -83,11 +103,16 @@ textE[0].innerHTML=getTextE.innerHTML
   function myFunction5() {
     var txt;
     var antwort = prompt("wähle deine Antwort\na:roter Stier \nb:roter Kuh " );
-    if (antwort == "b") {
-      txt = "falsche Antwort Tschüss machst gut nicht viel essen du kriegst Bauchschmerzen.";
-    } else {
+    if(antwort=="a") {
+      var background1 =document.getElementById('item5');
+    background1.style.background='url(smiley-163510__340.jpg)'
       txt ="Richtig du bist stark ,hast du Red Bull getrunken  !";
     }
+    else{
+      var background1 =document.getElementById('item5');
+      background1.style.background='url(smiley-3058590__340.jpg)'
+      txt = "falsche Antwort Tschüss machst gut nicht viel essen du kriegst Bauchschmerzen.";
+    } 
     document.getElementById("text5").innerHTML = txt;
   }
 
@@ -101,11 +126,16 @@ textF[0].innerHTML=getTextF.innerHTML
   function myFunction6() {
     var txt;
     var antwort = prompt("wähle deine Antwort\na:Auf Bäumen \nb:Am Strand " );
-    if (antwort == "b") {
-      txt = "falsche Antwort Tschüss machst gut nicht viel essen du kriegst Bauchschmerzen.";
-    } else {
+    if(antwort=="a") {
+      var background1 =document.getElementById('item6');
+    background1.style.background='url(smiley-163510__340.jpg)'
       txt ="Richtig du bist stark ,hast du Red Bull getrunken !";
     }
+    else {
+      var background1 =document.getElementById('item6');
+      background1.style.background='url(smiley-3058590__340.jpg)'
+      txt = "falsche Antwort Tschüss machst gut nicht viel essen du kriegst Bauchschmerzen.";
+    } 
     document.getElementById("text6").innerHTML = txt;
   }
   
@@ -119,11 +149,16 @@ textG[0].innerHTML=getTextG.innerHTML
   function myFunction7() {
     var txt;
     var antwort = prompt("wähle deine Antwort\na:die Würze \nb:die Wurzel " );
-    if (antwort == "b") {
-      txt = "falsche Antwort Tschüss machst gut nicht viel essen du kriegst Bauchschmerzen.";
-    } else {
+    if(antwort=="a") {
+      var background1 =document.getElementById('item7');
+    background1.style.background='url(smiley-163510__340.jpg)'
       txt ="Richtig du bist stark ,hast du Red Bull getrunken !";
     }
+    else {
+      var background1 =document.getElementById('item7');
+      background1.style.background='url(smiley-3058590__340.jpg)'
+      txt = "falsche Antwort Tschüss machst gut nicht viel essen du kriegst Bauchschmerzen.";
+    } 
     document.getElementById("text7").innerHTML = txt;
   } 
 var textH = document.getElementsByClassName('item8')
@@ -136,10 +171,17 @@ textH[0].innerHTML=getTextH.innerHTML
   function myFunction8() {
     var txt;
     var antwort = prompt("wähle deine Antwort\na:27 \nb:22 " );
-    if (antwort == "b") {
-      txt = "falsche Antwort Tschüss machst gut nicht viel essen du kriegst Bauchschmerzen.";
-    } else {
+    if(antwort="a") {
+      var background1 =document.getElementById('item8');
+    background1.style.background='url(smiley-163510__340.jpg)'
       txt ="Richtige Antwort mach weiter gleich geschafft !";
     }
+    else{
+      var background1 =document.getElementById('item8');
+      background1.style.background='url(smiley-3058590__340.jpg)'
+      txt = "falsche Antwort Tschüss machst gut nicht viel essen du kriegst Bauchschmerzen.";
+    } 
     document.getElementById("text8").innerHTML = txt;
   }  
+
+  
