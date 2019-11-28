03514f1f830ae280140127f4ba8588c1ac34449a
mohammad saied taleb <mohammadsolingen@gmail.com>
2019-11-28 10:11:11 +0100

    new

---
+++ 1/README.md
@@ -0,0 +1,17 @@
+# Calculator
+
+Schreib eine Taschenrechner-App die folgende Operationen ausführen kann:
+
+- Addition
+- Subtraktion
+- Multiplikation
+- Division
+- Modulo
+
+1. Schreib eine Funktion, die zwei Zahlen je nach Operation berechnet.
+
+1. **Optional:** Du kannst Bootstrap benutzen um die App zu stylen.
+
+> Orientiere dich an dieser Abbildung fürs Aussehen der App.
+
+![demo](demo.gif)
---
+++ 1/js.js
@@ -0,0 +1,18 @@
+
+var arr = ["+","-","*","/"]
+
+var var1= document.getElementById("var1")
+var var11=document.getElementById("var11")
+var result1 =document.getElementById("result1")
+function addieren(){
+result1.value=Number(var1.value) +Number(var11.value) 
+}
+
+var var2= document.getElementById("var2")
+var var22=document.getElementById("var22")
+var result2 =document.getElementById("result2")
+function reduzieren(){
+result2.value=Number(var2.value) -Number(var22.value) 
+}
+
+
